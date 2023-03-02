2a874b6f1452f1b4f1b3face71439435a9e894f8
saif-qureos <saif@qureos.com>
2023-03-02 20:03:30 +0500

    added some logs
    added some logs

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const route53 = new Route53();
 const core = require('@actions/core');
 
 async function upsertRecords(domainName, geoCodes, loadBalancerDns, loadBalancerHostedZoneId, route53HostedZoneId) {
+  console.log(`Upserting records for ${domainName}`);
+
   const records = await getRecordsByDomainName(domainName, route53HostedZoneId);
+  console.log(`Fetched ${records.length} records for ${domainName}`);
+
   const recordsToDelete = records.filter(record => {
     const isDefaultRecord = record.Type === 'A' && record.Name === domainName && record.AliasTarget && record.AliasTarget.DNSName === loadBalancerDns;
     const isGeoRecord = record.Type === 'A' && record.AliasTarget && record.AliasTarget.DNSName === loadBalancerDns && geoCodes.includes(record.GeoLocation.ContinentCode || record.GeoLocation.CountryCode);
     return isGeoRecord && !isDefaultRecord;
   });
-
+  console.log(recordsToDelete);
+  console.log(`Deleting ${recordsToDelete.length} records for ${domainName}`);
   // This code will first check if the record is the default record (the one with the domain name) and ignore it. For other records, it will check if they are geolocation records and include them only if they are not the default record.
   await Promise.all(recordsToDelete.map(record => deleteRecord(route53HostedZoneId, record)));
-
+  
+  console.log(`Upserting ${geoCodes.length} records for ${domainName}`);
 
   geoCodes.map(async code => {
     const GeoLocation = code.length === 2 ? { CountryCode: code } : { ContinentCode: code };
